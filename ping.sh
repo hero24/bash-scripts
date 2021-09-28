cded36f962a27abb4f91be36b83b766d5441aa8d
hero24 <hero24@interia.pl>
2021-09-28 10:47:19 +0100

    Update ping.sh
    show only the ones alive

--- ping.sh
+++ ping.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 # Pings all machines on given subnetwork 
 # eg. (123.145.231.) through given iterface
+# and output only the ones alive
 #
 # "Holding onto anger is like drinking poison and expecting the other person to die."
 # ~ Buddha
@@ -12,7 +13,7 @@ then
 else
         for x in `seq 1 254`
         do
-                ping -c 1 $1.$x -I $2
+                ping -c 1 $1.$x -I $2 | grep "64 bytes"
         done
 fi
 # Author: hero24
